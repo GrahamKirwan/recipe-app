902fb90d0b7276594450f8db306e7cd046ecc914
graham.kirwan4@gmail.com <graham.kirwan4@gmail.com>
2021-02-27 14:30:11 -0500

    added hashevent listeners

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,10 @@
+import icons from 'url:../img/icons.svg';
+
+// Import installed npm packages to will polyfill (support old browsers by changing async await etc to older syntax)
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -12,12 +19,29 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const spinnerHTML = `<div class="spinner">
+  <svg>
+    <use href="${icons}#icon-loader"></use>
+  </svg>
+</div> `
+parentEl.innerHTML = '';
+parentEl.insertAdjacentHTML('afterbegin', spinnerHTML);
+}
+
 
 const myFunction = async function () {
   try {
 
+    let id = window.location.hash.slice(1);
+    console.log(id);
+
+    if(!id) return;
+
+    renderSpinner(recipeContainer);
+
     // 1. Receive recipe from API call
-    const res = await fetch('https://forkify-api.herokuapp.com/api/get?rId=2fdcab');
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/get?rId=${id}`);
     const data = await res.json();
 
     if(!res.ok) throw new Error(`${res.status} ${data.message}`);
@@ -43,7 +67,7 @@ const myFunction = async function () {
     // 2. Display recipe 
 
 
-    let recipeHTML = `<figure class="recipe__fig">
+    let markup = `<figure class="recipe__fig">
                         <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
                         <h1 class="recipe__title">
                           <span>${recipe.title}</span>
@@ -53,14 +77,14 @@ const myFunction = async function () {
                       <div class="recipe__details">
                         <div class="recipe__info">
                           <svg class="recipe__info-icon">
-                            <use href="src/img/icons.svg#icon-clock"></use>
+                            <use href="${icons}#icon-clock"></use>
                           </svg>
                           <span class="recipe__info-data recipe__info-data--minutes">45</span>
                           <span class="recipe__info-text">minutes</span>
                         </div>
                         <div class="recipe__info">
                           <svg class="recipe__info-icon">
-                            <use href="src/img/icons.svg#icon-users"></use>
+                            <use href="${icons}#icon-users"></use>
                           </svg>
                           <span class="recipe__info-data recipe__info-data--people">4</span>
                           <span class="recipe__info-text">servings</span>
@@ -68,12 +92,12 @@ const myFunction = async function () {
                           <div class="recipe__info-buttons">
                             <button class="btn--tiny btn--increase-servings">
                               <svg>
-                                <use href="src/img/icons.svg#icon-minus-circle"></use>
+                                <use href="${icons}#icon-minus-circle"></use>
                               </svg>
                             </button>
                             <button class="btn--tiny btn--increase-servings">
                               <svg>
-                                <use href="src/img/icons.svg#icon-plus-circle"></use>
+                                <use href="${icons}#icon-plus-circle"></use>
                               </svg>
                             </button>
                           </div>
@@ -81,12 +105,12 @@ const myFunction = async function () {
 
                         <div class="recipe__user-generated">
                           <svg>
-                            <use href="src/img/icons.svg#icon-user"></use>
+                            <use href="${icons}#icon-user"></use>
                           </svg>
                         </div>
                         <button class="btn--round">
                           <svg class="">
-                            <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+                            <use href="${icons}#icon-bookmark-fill"></use>
                           </svg>
                         </button>
                       </div>
@@ -95,12 +119,18 @@ const myFunction = async function () {
                         <h2 class="heading--2">Recipe ingredients</h2>
                         <ul class="recipe__ingredient-list">
 
+
+                        ${recipe.ingredients.map( ing => {
+                          return `
                           <li class="recipe__ingredient">
-                            <svg class="recipe__icon">
-                              <use href="src/img/icons.svg#icon-check"></use>
-                            </svg>
-                            <div class="recipe__quantity">${recipe.ingredients[0]}</div>
-                          </li>
+                          <svg class="recipe__icon">
+                            <use href="${icons}#icon-check"></use>
+                          </svg>
+                          <div class="recipe__quantity">${ing}</div>
+                        </li>
+                          `
+                        }).join('')}
+
 
                         </ul>
                       </div>
@@ -119,13 +149,14 @@ const myFunction = async function () {
                         >
                           <span>Directions</span>
                           <svg class="search__icon">
-                            <use href="src/img/icons.svg#icon-arrow-right"></use>
+                            <use href="${icons}#icon-arrow-right"></use>
                           </svg>
                         </a>
                       </div>
   `
 
-  recipeContainer.insertAdjacentHTML('afterbegin', recipeHTML);
+        recipeContainer.innerHTML = '';
+        recipeContainer.insertAdjacentHTML('afterbegin', markup);
 
   } catch(err) {
       alert(err);
@@ -136,3 +167,6 @@ const myFunction = async function () {
 
 
 myFunction();
+
+window.addEventListener('hashchange', myFunction);
+window.addEventListener('load', myFunction);
